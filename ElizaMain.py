bef28a5beb9ac3f3be0f4ec9d1c5186b9fe096d3
Mike Pastor <86858904+mikepastor12@users.noreply.github.com>
2023-02-03 15:44:22 -0500

    Object Model updates

--- ElizaMain.py
+++ ElizaMain.py
@@ -7,30 +7,51 @@
 
 
 from Eliza import Eliza
-import SoundHeader as sh   # Our voice commands
 
+from SoundObject import SoundObject  # Our voice commands
+from OpenAIObject import OpenAIObject  # Our Open AI  commands
 
-# Our robot is instantiated here
+# Our robot and its components are instantiated here
 #
 eliza = Eliza()
 
+ai = OpenAIObject()
+
+so = SoundObject()
+
+###############################################
 print('Eliza Rogerian Therapist is running!')
 
-sh.speak('Hello I am Eliza the Rogerian Therapist ')
+so.speak('Hello I am Eliza the Rogerian Therapist ')
+#
+# while True:
+#     # message = input("? ")
+#     # parse as list
+#     # query = sh.parseCommand().lower().split()
+#     query = so.parseCommand().lower()
+#
+#     ints = eliza.predict_class( query )
+#
+#     res, tag = eliza.get_response(  ints, eliza.elizaIntents )
+#     so.speak( res )
+#
+#     if tag == 'goodbye':
+#         exit(0)
 
+
+# OpenAI version
+#
 while True:
-    # message = input("? ")
-    # parse as list
-    # query = sh.parseCommand().lower().split()
-    query = sh.parseCommand().lower()
 
-    ints = eliza.predict_class( query )
+    query = so.parseCommand().lower()
 
-    res, tag = eliza.get_response(  ints, eliza.elizaIntents )
-    sh.speak( res )
-
-    if tag == 'goodbye':
+    if query.lower() == 'goodbye':
+        so.speak("Goodbye Michael - see you soon")
         exit(0)
+
+    response = ai.do_completion( query )
+
+    so.speak( response )
 
 print('Done...')
 
---
+++ SoundObject.py
@@ -0,0 +1,84 @@
+##########################################################
+#       SoundObject.py
+#
+#
+#       pip install speechrecognition
+#       pip install pyttsx3
+#
+#           pip install wikipedia
+#           pip install wolframalpha
+#           pip install pyaudio
+
+#     sudo apt install espeak
+#      sudo apt install portaudio19-dev python3-pyaudio
+
+#       speaker-test -c2 -twav -l7
+
+#       Mike Pastor - January 27, 2023
+
+
+from datetime import datetime
+
+import speech_recognition as sr
+
+
+import pyttsx3
+
+# import webbrowser
+#  import wikipedia
+#  import wolframalpha
+
+#  import pyaudio
+
+class SoundObject():
+
+    print('####### Pennwick Rover  - Starting up...')
+    #  Speech engine
+    #
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+
+    #  0 - male voice 1 = female
+    engine.setProperty('voice', voices[0].id )
+
+    activationWord = 'computer'
+
+
+    def speak(self, text, rate=120):
+        self.engine.setProperty('rate', rate)
+        self.engine.say(text)
+        self.engine.runAndWait()
+
+    def parseCommand(self):
+        listener = sr.Recognizer()
+
+        listener.dynamic_energy_threshold = False
+
+        print('Listening...')
+
+        with sr.Microphone() as source:
+
+            listener.pause_threshold = 2
+            print('How can I help you? ')
+            input_speech = listener.listen(source)
+            # , timeout=10.0
+
+            try:
+                print('Recognizing speech...')
+                query = listener.recognize_google(input_speech, language='en_us')
+                print('Google interprets as - ', query)
+            except Exception as exception:
+                print('Sorry - didn''t catch that')
+                self.speak('Sorry Michael - didn''t catch that')
+                print(exception)
+                return 'None'
+
+            return query
+
+########################################################################
+#   main loop
+#
+# if __name__ == '__main__':
+#
+#     speak('Hello Brave New World! -Welcome to the 21st century')
+#     print('####### DONE! ')
